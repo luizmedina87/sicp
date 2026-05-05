a94a2ff1ab3dee41326c97cf8b7a30c14a74c8c9
Luiz Medina <luizmedina87@gmail.com>
2026-05-05 10:54:40 -0400

    add unit test to exercise 1.4

--- chapter-1/exercise-1-4.rkt
+++ chapter-1/exercise-1-4.rkt
@@ -10,9 +10,17 @@ following procedure:
 (define (a-plus-abs-b a b)
   ((if (> b 0) + -) a b))
 
+; TEST
+(= (a-plus-abs-b 3 2) 5) ; b positive, returns a + b
+(= (a-plus-abs-b 3 -2) 5) ; b negative, returns a - b = a + |b|
+(= (a-plus-abs-b 0 0) 0) ; both zero
+(= (a-plus-abs-b 3 0) 3) ; b is zero
+(= (a-plus-abs-b -3 2) -1) ; a negative, b positive
+(= (a-plus-abs-b -3 -2) -1) ; a negative, b negative
+
 #|
 ANSWER: The function evaluates the combination (+ a b) if b > 0, 
 and (- a b) otherwise. This is equivalente to returning
 a + |b| since it makes b positive in case it is a negative
 number.
-|# 
+|#
