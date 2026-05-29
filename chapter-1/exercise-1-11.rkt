5087f0950345a730b061110076ed773cd1aa6be7
Luiz Medina <luizmedina87@gmail.com>
2026-05-29 10:48:33 -0400

    finish part 1/2 of exercise 1.11

--- chapter-1/exercise-1-11.rkt
+++ chapter-1/exercise-1-11.rkt
@@ -10,4 +10,21 @@ process. Write a procedure that computes f by means of an
 iterative process. |#
 
 
+(define (f-rec n)
+  (if (< n 3)
+      n
+      (+ (f-rec (- n 1))
+         (* 2 (f-rec (- n 2)))
+         (* 3 (f-rec (- n 3))))))
 
+
+
+
+
+; TESTS
+(display "(f-rec 0): ") (display (= (f-rec 0) 0)) (newline)
+(display "(f-rec 1): ") (display (= (f-rec 1) 1)) (newline)
+(display "(f-rec 2): ") (display (= (f-rec 2) 2)) (newline)
+(display "(f-rec 3): ") (display (= (f-rec 3) 4)) (newline)
+(display "(f-rec 4): ") (display (= (f-rec 4) 11)) (newline)
+(display "(f-rec 5): ") (display (= (f-rec 5) 25)) (newline)
