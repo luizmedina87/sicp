837f8ff47041a32b2a7907b5c9ff03f9b9676156
Luiz Medina <luizmedina87@gmail.com>
2026-05-29 12:11:03 -0400

    finish exercise 1.11

--- chapter-1/exercise-1-11.rkt
+++ chapter-1/exercise-1-11.rkt
@@ -10,6 +10,8 @@ process. Write a procedure that computes f by means of an
 iterative process. |#
 
 
+; ANSWER:
+; (RECURSIVE PROCESS)
 (define (f-rec n)
   (if (< n 3)
       n
@@ -17,14 +19,38 @@ iterative process. |#
          (* 2 (f-rec (- n 2)))
          (* 3 (f-rec (- n 3))))))
 
-
-
-
-
 ; TESTS
+(newline)
+(display "Tests for the recursive process procedure")
+(newline)
+(display "-----------------------------------------")
+(newline)
 (display "(f-rec 0): ") (display (= (f-rec 0) 0)) (newline)
 (display "(f-rec 1): ") (display (= (f-rec 1) 1)) (newline)
 (display "(f-rec 2): ") (display (= (f-rec 2) 2)) (newline)
 (display "(f-rec 3): ") (display (= (f-rec 3) 4)) (newline)
 (display "(f-rec 4): ") (display (= (f-rec 4) 11)) (newline)
 (display "(f-rec 5): ") (display (= (f-rec 5) 25)) (newline)
+
+
+; ANSWER:
+; (ITERATIVE PROCESS)
+(define (f-iter n) ; iterative process
+  (define (f-iter-rec a b c count) ; recursive procedure
+    (if (= count 0)
+        c
+        (f-iter-rec (+ a (* 2 b) (* 3 c)) a b (- count 1))))
+  (f-iter-rec 2 1 0 n))
+
+; TESTS
+(newline)
+(display "Tests for the iterative process procedure")
+(newline)
+(display "-----------------------------------------")
+(newline)
+(display "(f-iter 0): ") (display (= (f-iter 0) 0)) (newline)
+(display "(f-iter 1): ") (display (= (f-iter 1) 1)) (newline)
+(display "(f-iter 2): ") (display (= (f-iter 2) 2)) (newline)
+(display "(f-iter 3): ") (display (= (f-iter 3) 4)) (newline)
+(display "(f-iter 4): ") (display (= (f-iter 4) 11)) (newline)
+(display "(f-iter 5): ") (display (= (f-iter 5) 25)) (newline)
