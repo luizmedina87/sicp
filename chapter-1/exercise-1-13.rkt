9be3cd4a35c1898d128f3239a46dbe441102dddf
Luiz Medina <luizmedina87@gmail.com>
2026-06-01 10:35:16 -0400

    add exercise 1.13

---
+++ chapter-1/exercise-1-13.rkt
@@ -0,0 +1,10 @@
+#lang sicp
+
+#| Exercise 1.13:
+
+Prove that Fib(n) is the closest integer to φⁿ/√5,
+where φ = (1 + √5)/2.
+
+Hint: Let ψ = (1 − √5)/2. Use induction and the definition
+of the Fibonacci numbers (see Section 1.2.2) to prove that
+Fib(n) = (φⁿ − ψⁿ)/√5. |#
