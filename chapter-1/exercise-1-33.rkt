1294e661a2c481043d0f1bbd86cad71203085e82
Luiz Medina <luizmedina87@gmail.com>
2026-08-04 13:56:45 -0400

    refactor exercise 1.33

--- chapter-1/exercise-1-33.rkt
+++ chapter-1/exercise-1-33.rkt
@@ -1,5 +1,9 @@
 #lang sicp
 
+(#%require "./exercise-1-28.rkt") ; fast-prime?
+(#%require "./exercise-1-20.rkt") ; gcd
+(#%provide (all-defined))
+
 
 #| Exercise 1.33: You can obtain an even more general ver-
 sion of accumulate (Exercise 1.32) by introducing the no-
@@ -20,12 +24,6 @@ b. the product of all the positive integers less than n that
    are relatively prime to n (i.e., all positive integers i < n
    such that GCD(i, n) = 1). |#
 
-(#%require "./exercise-1-28.rkt") ; fast-prime?
-(#%require "./exercise-1-20.rkt") ; gcd
-
-; Adapting fast-prime? for use here. 40 is the number of times to
-; test the number for primality, which probabilistically ensures
-; the number is prime for all relavant purposes.
 
 ; ANSWER
 
@@ -33,8 +31,8 @@ b. the product of all the positive integers less than n that
 (define (inc x) (+ x 1))
 (define (identity x) x)
 (define (square x) (* x x))
-(define (even? n) (= (remainder n 2) 0))
 
+; Ex. 33
 (define (accumulate-rec combiner null-value term a next b)
   (if (> a b)
       null-value
@@ -46,7 +44,13 @@ b. the product of all the positive integers less than n that
                                 next
                                 b))))
 
-(define (filtered-accumulate-rec combiner predicate null-value term a next b)
+(define (filtered-accumulate-rec combiner 
+                                 predicate 
+                                 null-value 
+                                 term 
+                                 a 
+                                 next 
+                                 b)
   (if (> a b)
       null-value
       (if (predicate a)
@@ -67,12 +71,18 @@ b. the product of all the positive integers less than n that
                                    b))))
 
 
+; Ex. 33.a
 (define (sum-squares-primes a b)
+   ; Adapting fast-prime? for use here. 40 is the number of times 
+   ; to test the number for primality, which probabilistically 
+   ; ensures the number is prime for all relavant purposes.
 	(define (prime? n)
 		(cond ((< n 2) false)
 					(else (fast-prime? n 40))))
   (filtered-accumulate-rec + prime? 0 square a inc b))
 
+
+; Ex. 33.b
 (define (product-relative-prime n)
 	(define (relative-prime i)
 		(if (= (gcd i n) 1)
@@ -80,5 +90,3 @@ b. the product of all the positive integers less than n that
 				false))
 	(filtered-accumulate-rec * relative-prime 1 identity 2 inc n))
 
-
-(#%provide (all-defined))
