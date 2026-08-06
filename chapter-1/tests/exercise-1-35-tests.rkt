9b88484c3e071048094417a279a77fa766e9078e
Luiz Medina <luizmedina87@gmail.com>
2026-08-06 15:30:43 -0400

    use test module on exercise 1.35

--- chapter-1/tests/exercise-1-35-tests.rkt
+++ chapter-1/tests/exercise-1-35-tests.rkt
@@ -1,36 +1,8 @@
 #lang sicp
 
 (#%require "../exercise-1-35.rkt")
+(#%require "../lib/test.rkt")
 
-;; =============================================================================
-;; Test Harness
-;; =============================================================================
-
-(define (assert-equal test-name expected actual)
-  (if (equal? expected actual)
-      (begin
-        (display " [PASS] ")
-        (display test-name)
-        (newline))
-      (begin
-        (display " [FAIL] ")
-        (display test-name)
-        (newline)
-        (display "    Expected: ") (display expected) (newline)
-        (display "    Actual:   ") (display actual)   (newline))))
-
-(define (assert-close test-name expected actual tolerance)
-  (if (< (abs (- expected actual)) tolerance)
-      (begin
-        (display " [PASS] ")
-        (display test-name)
-        (newline))
-      (begin
-        (display " [FAIL] ")
-        (display test-name)
-        (newline)
-        (display "    Expected approx: ") (display expected) (newline)
-        (display "    Actual:          ") (display actual)   (newline))))
 
 ;; =============================================================================
 ;; Helpers & Constants
